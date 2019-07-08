3a939533202119a3c2c7f588479119c8397d3cec
ARisa <ployarisa11@users.noreply.github.com>
2019-07-08 12:47:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if($method == 'POST'){
 	$json = json_decode($requestBody);
 
 	$text = $json->result->parameters->text;
-	
+
 
 	switch ($text) {
 		case 'hi':
@@ -24,7 +24,7 @@ if($method == 'POST'){
 			break;
 		
 		default:
-			$speech = "Sorry, I didnt get that. Please ask me something else.";
+			$speech = $text;
 			break;
 	}
 
