835a6ab27e1496e025ea1a69c730144667b5cc48
ARisa <ployarisa11@users.noreply.github.com>
2019-07-08 11:49:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php 
 
 $method = $_SERVER['REQUEST_METHOD'];
+$text = $json->result->parameters->text;
+echo json_encode($text);
 
 // Process only when method is POST
 if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 
-	$text = $json->result->parameters->text;
+	
 
 	switch ($text) {
 		case 'hi':
@@ -32,7 +34,7 @@ if($method == 'POST'){
 	$response->speech = $speech;
 	$response->displayText = $speech;
 	$response->source = "webhook";
-	echo json_encode($text);
+	
 }
 else
 {
