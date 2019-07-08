5a717de99c62b0a82e76f86791ffc469fa268b64
ARisa <ployarisa11@users.noreply.github.com>
2019-07-08 12:50:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,8 @@ if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 
-	$text = $json->result->parameters->text;
+
+	$text = $json->queryResult->parameters->text;
 
 
 	switch ($text) {
