eaad14ccdf281b645973ea7e215b2d5a8ee3a68e
ARisa <ployarisa11@users.noreply.github.com>
2019-07-08 20:45:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,44 +1,14 @@
-<?php 
-
-$method = $_SERVER['REQUEST_METHOD'];
-
-// Process only when method is POST
-if($method == 'POST'){
-	$requestBody = file_get_contents('php://input');
-	$json = json_decode($requestBody);
-
-
-	$text = $json->queryResult->parameters->text;
-
-
-	switch ($text) {
-		case 'hi':
-			$speech = "Hi, Nice to meet you";
-			break;
-
-		case 'bye':
-			$speech = "Bye, good night";
-			break;
-
-		case 'anything':
-			$speech = "Yes, you can type anything here.";
-			break;
-		
-		default:
-			$speech = $text;
-			break;
-	}
-
-	$fulfillmentText = $speech;
-	$response = new \stdClass();
-	$response->speech = $speech;
-	$response->displayText = $speech;
-	$response->source = "webhook";
-	echo json_encode($fulfillmentText);
-}
-else
-{
-	echo "Method not allowed";
-}
+<?php
+date_default_timezone_set(“Asia/Bangkok”);
+$date = date(“Y-m-d”);
+$time = date(“H:i:s”);
+$json = file_get_contents(‘php://input’);
+$request = json_decode($json, true);
+$queryText = $request[“queryResult”][“queryText”];
+$userId = $request[‘originalDetectIntentRequest’][‘payload’][‘data’][‘source’][‘userId’];
+$myfile = fopen(“log$date.txt”, “a”) or die(“Unable to open file!”);
+$log = $date.”-”.$time.”\t”.$userId.”\t”.$queryText.”\n”;
+fwrite($myfile,$log);
+fclose($myfile);
 
 ?>
