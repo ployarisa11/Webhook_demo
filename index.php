1dd4953c71535596d929d3f9f5c4b432e1ea46f4
ARisa <ployarisa11@users.noreply.github.com>
2019-07-09 10:00:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,6 @@ $method = $_SERVER['REQUEST_METHOD'];
 // Process only when method is POST
 if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
-	date_default_timezone_set(“Asia/Bangkok”);
-	$date = date(“Y-m-d”);
 	$json = json_decode($requestBody);
 
 
@@ -30,7 +28,17 @@ if($method == 'POST'){
 			$speech = $text;
 			break;
 	}
-
+	date_default_timezone_set(“Asia/Bangkok”);
+	$date = date(“Y-m-d”);
+	$time = date(“H:i:s”);
+	$json = file_get_contents(‘php://input’);
+	$request = json_decode($json, true);
+	$queryText = $request[“queryResult”][“queryText”];
+	$userId = $request[‘originalDetectIntentRequest’][‘payload’][‘data’][‘source’][‘userId’];
+	$myfile = fopen(“log$date.txt”, “a”) or die(“Unable to open file!”);
+	$log = $date.”-”.$time.”\t”.$userId.”\t”.$queryText.”\n”;
+	fwrite($myfile,$log);
+	fclose($myfile);
 	
 	$response = new \stdClass();
 	$response->speech = $speech;
