e78014534616c43692a191426cb22274edbf9ee6
ARisa <ployarisa11@users.noreply.github.com>
2019-07-09 22:08:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,49 +1,41 @@
 <?php 
-date_default_timezone_set("Asia/Bangkok");
+
 $method = $_SERVER['REQUEST_METHOD'];
 
 // Process only when method is POST
 if($method == 'POST'){
-	$date = date("Y-m-d");
-	$time = date("H:i:s");
 	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+
+
 	$text = $json->queryResult->parameters->text;
-	$request = json_decode($json, true);
-	$queryText = $request["queryResult"]["queryText"];
-	$userId = $request['originalDetectIntentRequest']['payload']['data']['source']['userId'];
-	
-	echo json_encode($time);
-
-	// $json = json_decode($requestBody);
 
 
-	
+	switch ($text) {
+		case 'hi':
+			$speech = "Hi, Nice to meet you";
+			break;
 
+		case 'bye':
+			$speech = "Bye, good night";
+			break;
 
-	// switch ($text) {
-	// 	case 'hi':
-	// 		$speech = "Hi, Nice to meet you";
-	// 		break;
-
-	// 	case 'bye':
-	// 		$speech = "Bye, good night";
-	// 		break;
-
-	// 	case 'anything':
-	// 		$speech = "Yes, you can type anything here.";
-	// 		break;
+		case 'anything':
+			$speech = "Yes, you can type anything here.";
+			break;
 		
-	// 	default:
-	// 		$speech = $queryText;
-	// 		break;
-	// }
+		default:
+			$speech = $text;
+			break;
+	}
 
 	
-	// $response = new \stdClass();
-	// $response->speech = $speech;
-	// $response->displayText = $speech;
-	// $response->source = "webhook";
-	// echo json_encode($response);
+	$response = new \stdClass();
+	$response->speech = $speech;
+	$response->displayText = $speech;
+	$response->source = "webhook";
+	echo json_encode($response);
+	$agent->reply('Hi, how can I help?');
 }
 else
 {
