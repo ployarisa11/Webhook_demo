c00e64a7d48f8bb8ec3fe125e7abe90f4df131d9
ARisa <ployarisa11@users.noreply.github.com>
2019-07-11 16:33:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if($method == 'POST'){
 	$json = json_decode($requestBody);
 
 	$text = $json->queryResult->parameters->text;
-	$responseId = $json->responseId;
+	// $responseId = $json->responseId;
 
 	switch ($text) {
 		case 'hi':
@@ -24,7 +24,7 @@ if($method == 'POST'){
 			break;
 		
 		default:
-			$speech = $responseId;
+			$speech = 'pppp';
 			break;
 	}
 
@@ -32,7 +32,10 @@ if($method == 'POST'){
 	$response->speech = $speech;
 	$response->displayText = $speech;
 	$response->source = "webhook";
+
 	echo json_encode($response);
+	$fulfillmentText = $json->queryResult->$speech;
+	echo $fulfillmentText;
 }
 else
 {
