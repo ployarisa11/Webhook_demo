2e5d05ac4801f7eedc25825309f4b36eef445200
ARisa <ployarisa11@users.noreply.github.com>
2019-07-11 23:05:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
 <?php 
 
-$method = $_SERVER['REQUEST_METHOD'];
+// $method = $_SERVER['REQUEST_METHOD'];
 
-// Process only when method is POST
-if($method == 'POST'){
+// // Process only when method is POST
+// if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 
 	$text = $json->queryResult->parameters->text;
 	// $responseId = $json->responseId;
 
-	switch ($text) {
-		case 'hi':
-			$speech = "Hi, Nice to meet you";
-			break;
+	// switch ($text) {
+	// 	case 'hi':
+	// 		$speech = "Hi, Nice to meet you";
+	// 		break;
 
-		case 'bye':
-			$speech = "Bye, good night";
-			break;
+	// 	case 'bye':
+	// 		$speech = "Bye, good night";
+	// 		break;
 
-		case 'anything':
-			$speech = "Yes, you can type anything here.";
-			break;
+	// 	case 'anything':
+	// 		$speech = "Yes, you can type anything here.";
+	// 		break;
 		
-		default:
-			$speech = 'pppp';
-			break;
-	}
+	// 	default:
+	// 		$speech = 'pppp';
+	// 		break;
+	// }
 
 	// $response = new \stdClass();
 	// $response->speech = $speech;
@@ -34,17 +34,18 @@ if($method == 'POST'){
 	// $response->source = "webhook";
 
 	$response = "It is $temperature degrees with";
-
 	// echo json_encode($response);
+
+	//show user
 	$fulfillment = array(
        "fulfillmentText" => $response
    );
 
    echo(json_encode($fulfillment));
-}
-else
-{
-	echo "Method not allowed";
-}
+// }
+// else
+// {
+// 	echo "Method not allowed";
+// }
 
 ?>
