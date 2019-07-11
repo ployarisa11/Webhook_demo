037df73538c1c09a52a6e5dc14ca488cac84cc04
ARisa <ployarisa11@users.noreply.github.com>
2019-07-11 22:51:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,64 +1,43 @@
 <?php 
 
-// $method = $_SERVER['REQUEST_METHOD'];
+$method = $_SERVER['REQUEST_METHOD'];
 
-// // Process only when method is POST
-// if($method == 'POST'){
-// 	$requestBody = file_get_contents('php://input');
-// 	$json = json_decode($requestBody);
+// Process only when method is POST
+if($method == 'POST'){
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
 
-// 	$text = $json->queryResult->parameters->text;
-// 	// $responseId = $json->responseId;
+	$text = $json->queryResult->parameters->text;
+	// $responseId = $json->responseId;
 
-// 	switch ($text) {
-// 		case 'hi':
-// 			$speech = "Hi, Nice to meet you";
-// 			break;
+	switch ($text) {
+		case 'hi':
+			$speech = "Hi, Nice to meet you";
+			break;
 
-// 		case 'bye':
-// 			$speech = "Bye, good night";
-// 			break;
+		case 'bye':
+			$speech = "Bye, good night";
+			break;
 
-// 		case 'anything':
-// 			$speech = "Yes, you can type anything here.";
-// 			break;
+		case 'anything':
+			$speech = "Yes, you can type anything here.";
+			break;
 		
-// 		default:
-// 			$speech = 'pppp';
-// 			break;
-// 	}
+		default:
+			$speech = 'pppp';
+			break;
+	}
 
-// 	$response = new \stdClass();
-// 	$response->speech = $speech;
-// 	$response->displayText = $speech;
-// 	$response->source = "webhook";
+	$response = new \stdClass();
+	$response->speech = $speech;
+	$response->displayText = $speech;
+	$response->source = "webhook";
 
-// 	echo json_encode($response);
-// }
-// else
-// {
-// 	echo "Method not allowed";
-// }
-
-function processMessage($update) {
-    if($update["result"]["action"] == "sayHello"){
-        sendMessage(array(
-            "source" => $update["result"]["source"],
-            "speech" => "Hello from webhook",
-            "displayText" => "Hello from webhook",
-            "contextOut" => array()
-        ));
-    }
+	echo json_encode($response);
 }
-
-function sendMessage($parameters) {
-    echo json_encode($parameters);
-}
-
-$update_response = file_get_contents("php://input");
-$update = json_decode($update_response, true);
-if (isset($update["result"]["action"])) {
-    processMessage($update);
+else
+{
+	echo "Method not allowed";
 }
 
 ?>
